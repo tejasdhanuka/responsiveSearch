7d29544e7aa10d51bd3177fc7cd48eabb14fe0e9
Tejas, Dhanuka <ts-dhanuka.tejas@rakuten.com>
2020-02-18 13:34:08 +0900

    Initial project folder commit

---
+++ ResponsiveSearch/ResponsiveSearch/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ResponsiveSearch
+//
+//  Created by Tejas, Dhanuka on 2020/02/18.
+//  Copyright © 2020 Tejas. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
